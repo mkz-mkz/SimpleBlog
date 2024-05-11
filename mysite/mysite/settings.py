4ca6f638a904ac58fc875927ed14f8cd68d81d1a
mkz-mkz <maxim.krysov@gmail.com>
2024-05-11 12:51:08 +0300

    Updated env template and settings variables including PostgreSQL variables

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
+from os import getenv
 from pathlib import Path
 
 
@@ -21,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$eimy514j&02%w+rp90vudwxqwxunnv8(0e36!i*4q_k#9ovm-'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
